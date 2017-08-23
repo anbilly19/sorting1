0d0f5d097af0bcb383ee105df01cd52ae5016f6d
student <student@10.12.1.207>
2017-08-23 09:52:45 +0530

    Bubble sort

---
+++ src/Bubblesort.java
@@ -0,0 +1,36 @@
+
+		import java.util.Scanner;
+		public class Bubblesort {
+
+			public static void main(String[] args) {
+				int n,i,j,temp;
+				Scanner sc= new Scanner(System.in);
+				System.out.println("Enter size of array");
+				n=sc.nextInt();
+				int[] arr=new int[n];
+				System.out.println("Enter "+n+"integer:");
+				for(i=0;i<n;i++){
+					arr[i]=sc.nextInt();
+				}
+				for(i=0;i<n-1;i++){
+					for(j=0;j<n-i-1;j++){
+						if(arr[j]>arr[j+1]){
+							temp=arr[j];
+							arr[j]=arr[j+1];
+							arr[j+1]=temp;
+							
+						}
+					}
+				}
+				System.out.println("Sorted list of integers:");
+				for(i=0;i<n;i++){
+					System.out.println(arr[i]);
+				}
+			}
+
+		
+
+
+	}
+
+
